4d7f69fa5eddcccb370adce846cb795518e03f92
lamhiep2108 <hiepnothip@gmail.com>
2021-08-05 22:45:27 +0700

    'l'

---
+++ src/main/java/com/example/demospringboott/DemoSpringBoottApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringboott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBoottApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBoottApplication.class, args);
+    }
+
+}
